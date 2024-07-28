8acf57c2a6fe992d03c7d191a1f2d54fc6f87b11
Akhil Waghmare <akhilwaghmare@gmail.com>
2024-07-27 18:24:02 -0700

    Initial working version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,61 @@
+"use client";
+
+import { motion, useSpring, useTime, useTransform } from "framer-motion";
+import { useEffect } from "react";
+
 export default function Home() {
+  const time = useTime();
+
+  const rotate = useTransform(time, [0, 2000], [0, 360], {
+    clamp: false,
+  });
+  const rotatingBg = useTransform(rotate, (r) => {
+    return `conic-gradient(from ${r}deg, #ff4545, #00ff99, #006aff, #ff0095, #ff4545)`;
+  });
+
+  const pulse = useSpring(0, { damping: 0, mass: 5 });
+  const pulsingBg = useTransform(pulse, (r) => {
+    return `blur(${r}px)`;
+  });
+  useEffect(() => {
+    pulse.set(2);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Starter code
+    <main className="flex min-h-screen flex-col items-center justify-center gap-20 p-24 bg-neutral-900 text-neutral-100">
+      {/* Rotating gradient */}
+      <div className="relative">
+        <button className="relative bg-neutral-700 px-3 py-2 rounded-md hover:bg-neutral-800 transition-colors duration-200 z-10">
+          Subscribe to Built With Code
+        </button>
+        <motion.div
+          className="absolute -inset-[1px] bg-yellow-100 rounded-md"
+          style={{
+            background: rotatingBg,
+          }}
+        />
+      </div>
+      {/* Pulsing gradient */}
+      <div className="relative">
+        <button className="relative bg-neutral-700 px-3 py-2 rounded-md hover:bg-neutral-800 transition-colors duration-200 z-10">
+          Subscribe to Built With Code
+        </button>
+        <motion.div
+          className="absolute -inset-[1px] bg-yellow-100 rounded-md"
+          style={{
+            background:
+              "conic-gradient(#ff4545, #00ff99, #006aff, #ff0095, #ff4545)",
+          }}
+        />
+        <motion.div
+          className="absolute -inset-[1px] bg-yellow-100 rounded-md opacity-50"
+          style={{
+            background:
+              "conic-gradient(#ff4545, #00ff99, #006aff, #ff0095, #ff4545)",
+            filter: pulsingBg,
+          }}
+        />
+      </div>
     </main>
   );
 }
