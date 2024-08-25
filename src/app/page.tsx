b2ddad6c4a20af184351a8e46891588500b4c64b
Akhil Waghmare <akhilwaghmare@gmail.com>
2024-08-25 11:10:58 -0700

    Add basic button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-20 p-24 bg-neutral-900 text-neutral-100">
-      Starter code
+      <button className="bg-neutral-700 px-3 py-2 rounded-md hover:bg-neutral-800 transition-colors duration-200">
+        Subscribe to Built With Code
+      </button>
     </main>
   );
 }
